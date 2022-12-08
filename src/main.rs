b7f3a1849606cb050469c878522c1ddd657e9af9
Jefferson Oliveira <jefferson.dev.insights@gmail.com>
2022-12-07 21:41:12 -0300

    write frequencies to csv file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use clap::Parser;
+use std::fs;
+use std::io::prelude::*;
 
 /// Calculates the acoustic transmission of duct elements
 #[derive(Parser, Debug)]
@@ -9,14 +11,27 @@ struct Cli {
     /// The high frequency limit
     #[arg(long)]
     high_frequency: i32,
+    /// Output file
+    #[arg(short, long)]
+    output: String,
 }
 
 fn main() {
-    println!("Hello, world!");
     println!("Sin: {}", (std::f32::consts::PI / 2f32).sin());
     let args = Cli::parse();
 
     println!("Args: {:?}", args);
+
+    let mut file = match fs::File::create(&args.output) {
+        Ok(file) => file,
+        Err(_e) => panic!("Could not create the output file."),
+    };
+
+    for frequency in (args.lower_frequency..args.high_frequency).step_by(5) {
+        let csv_line = format!("{}, placeholder_value;\n", frequency).into_bytes();
+        file.write(&csv_line)
+            .expect("Coult not write to the output file.");
+    }
 
     println!(
         "Calculating from {} hz to {} hz",
